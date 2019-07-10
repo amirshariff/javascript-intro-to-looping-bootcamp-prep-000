dfc179a7ae98facbe933f1874e1acbd767c05f78
amir shariff <amirshariff@msn.com>
2019-07-10 11:22:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
-for (let i=0; i<25;i++){
- `I am ${i} strange loop${i===0 ? '' : 's'}.`
-}
+function forLoop(array){
+  for (let i=0; i<25;i++){
+ array.push(`I am ${i} strange loop${i===0 ? '' : 's'}.`)
+}}
